df49647535730adff90782d613c2f8a2148dbe01
allan516 <79980519+allan516@users.noreply.github.com>
2023-10-22 02:23:01 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,4 @@
 
-// function resposta() {
-//     const peso = document.getElementById('peso');
-//     const altura = document.getElementById('altura');
-
-//     const imc = Number(peso.value / (altura.value ** 2));
-//     const respostaNaTela = document.getElementById('resultado');
-    
-    
-    
-//     formNenviara.onclick = function (evento){
-//         evento.preventDefault();
-//        if (imc <= 0){
-//             respostaNaTela.innerHTML = '<p> numero invalido</p>';
-       
-//         } else if (imc < 18.5){
-//             respostaNaTela.innerHTML = '<p> Abaixo do peso</p>';
-//         } else if (imc >= 18.5 && imc <= 24.9) {
-//             respostaNaTela.innerHTML = '<p>Peso normal</p>';
-//         } else if (imc >= 25 && imc <=29.9){
-//             respostaNaTela.innerHTML = '<p>Sobrepeso</p>';
-//         } else if (imc >= 30 && imc <= 34.9){
-//             respostaNaTela.innerHTML = '<p>Obesidade grau 1</p>';
-//         } else if (imc >= 35 && imc <= 39.9){
-//             respostaNaTela.innerHTML = '<p>Obesidade grau 2</p>';
-//         } else if (imc >= 40) {
-//             respostaNaTela.innerHTML = '<p>Obesidade grau 3</p>';
-//         }
-//     };
-// }
-
 function resposta() {
     const peso = Number(document.getElementById('peso').value);
     const altura = Number(document.getElementById('altura').value);
